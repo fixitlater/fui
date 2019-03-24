5ef10c130f75549af8844a009a9609e301e4c8b3
fixitlater <fixitl8r@gmail.com>
2019-03-24 17:34:33 -0500

    Updating dist

--- dist/fui.nc.js
+++ dist/fui.nc.js
@@ -177,34 +177,3 @@
 		window.fui = fui();
 	}
 })(window, document);
-function a() { return fui.a.apply(null, arguments); }
-function button() { return fui.button.apply(null, arguments); }
-function canvas() { return fui.canvas.apply(null, arguments); }
-function div() { return fui.div.apply(null, arguments); }
-function h1() { return fui.h1.apply(null, arguments); }
-function h2() { return fui.h2.apply(null, arguments); }
-function h3() { return fui.h3.apply(null, arguments); }
-function h4() { return fui.h4.apply(null, arguments); }
-function h5() { return fui.h5.apply(null, arguments); }
-function h6() { return fui.h6.apply(null, arguments); }
-function header() { return fui.header.apply(null, arguments); }
-function i() { return fui.i.apply(null, arguments); }
-function input() { return fui.input.apply(null, arguments); }
-function label() { return fui.label.apply(null, arguments); }
-function li() { return fui.li.apply(null, arguments); }
-function ol() { return fui.ol.apply(null, arguments); }
-function optgroup() { return fui.optgroup.apply(null, arguments); }
-function option() { return fui.option.apply(null, arguments); }
-function p() { return fui.p.apply(null, arguments); }
-function pre() { return fui.pre.apply(null, arguments); }
-function select() { return fui.select.apply(null, arguments); }
-function span() { return fui.span.apply(null, arguments); }
-function table() { return fui.table.apply(null, arguments); }
-function thead() { return fui.thead.apply(null, arguments); }
-function tbody() { return fui.tbody.apply(null, arguments); }
-function tfoot() { return fui.tfoot.apply(null, arguments); }
-function tr() { return fui.tr.apply(null, arguments); }
-function th() { return fui.th.apply(null, arguments); }
-function td() { return fui.td.apply(null, arguments); }
-function ul() { return fui.ul.apply(null, arguments); }
-function form() { return fui.form.apply(null, arguments); }
